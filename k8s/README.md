151a0aa8871ac0040b0031ec6a50b64a4fe770ca
Andrei Duca <aduca@Andreis-MacBook-Pro.local>
2019-06-18 14:07:39 +0300

    Added support for the k8s deployment

--- k8s/README.md
+++ k8s/README.md
@@ -2,10 +2,12 @@ Reverse proxy k8s deployment
 ==================
 
 This k8s chart deploys the reverse proxy and two upstreams (customized nginx servers) for testing purposes.
-The connection to the upstreams is done via environment variables that reference cluster ip objects for each ustream
+The connection to the upstreams is done via environment variables that reference cluster ip objects for each upstream.
+The ingress controller is a NodePort - you need to get the minikube IP and the port listed under services (it should be a big port)
 
 Requirements
 ------------
+Minikube
 Helm
 Docker
 
--- sources/main.py
+++ sources/main.py
@@ -1,6 +1,7 @@
 import server
 import yaml
 import cProfile
+import os
 
 def read_config(config_file):
     with open(config_file, 'r') as ymlfile:
@@ -25,7 +26,22 @@ def main():
     service = server.Server()
     service.run(server_address, upstream_addresses, load_balancer_strategy)
 
+def main_k8s():
+    server_address = (os.environ['SERVER_ADDRESS'], int(os.environ['SERVER_PORT']))
+    load_balancer_strategy = os.environ['LOAD_BALANCER_STRATEGY']
+    upstream_one_address = (os.environ['UPSTREAM_ONE_ADDRESS'], int(os.environ['UPSTREAM_ONE_PORT']))
+    upstream_two_address = (os.environ['UPSTREAM_TWO_ADDRESS'], int(os.environ['UPSTREAM_TWO_PORT']))
+    upstream_addresses = [upstream_one_address, upstream_two_address]
+
+    service = server.Server()
+    service.run(server_address, upstream_addresses, load_balancer_strategy)
+
 
 if __name__ == '__main__':
+    if os.environ['K8S'] == 'true':
+        main_k8s()
+    else:
+        main()
     #cProfile.run('main()')
-    main()
+
+    
